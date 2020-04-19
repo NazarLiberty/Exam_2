ad95325926c28c7a5e7f5b7a203924cee719c8a6
NazarLiberty <NazarLiberty@gmail.com>
2020-04-19 16:21:33 +0300

    gallery completed

--- index.js
+++ index.js
@@ -22,6 +22,40 @@ $(document).ready(
     })
 
 )
+$(document).ready(
+    $('.n-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        slidesToShow: 3,
+        nextArrow: ' <img src="./img/arrRight.png" alt="" class="rightArr">',
+        prevArrow: ' <img src="./img/arrLeft.png" alt="" class="leftArr">',
+        dotsClass: 'news-dots',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: true,
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 568,
+                settings: {
+                    centerMode: false,
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    })
+)
 $('#burger').on('click', function () {
     $('#menu').addClass('h-menu__text--active');
 })
